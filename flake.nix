583dcd770257a8579c861123cbd651affbcee862
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2025-11-23 10:34:52 +0200

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Go Patterns";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.go
+        ];
+      };
+    };
+}
